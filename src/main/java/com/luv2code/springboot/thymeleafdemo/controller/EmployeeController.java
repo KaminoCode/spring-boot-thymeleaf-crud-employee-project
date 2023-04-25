1b762fb19d0f66dd937d6b090e351e3037f9d7d4
ozark-dev <jason@ozarkdev.com>
2023-04-24 23:42:34 -0500

    Added update functionality to EmployeeController and employee-form

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -47,6 +44,20 @@ public class EmployeeController {
 
 		theModel.addAttribute("employee", theEmployee);
 
+		return "employees/employee-form";
+	}
+
+	// add mapping for "/showFormForUpdate"
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+
+		// set employee in the model to prepopulate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form
 		return "employees/employee-form";
 	}
 
